6e510d3b6b8f0fdd19ac6eeb81932a079eca542f
soyuka <soyuka@gmail.com>
2018-05-10 20:44:46 +0200

    success

--- index.js
+++ index.js
@@ -1,62 +1,90 @@
+const runtime = require('browser.runtime')
 const hyperdrive = require('hyperdrive')
 const pump = require('pump')
-const {Duplex} = require('stream')
+const {Duplex, PassThrough, Transform} = require('stream')
 const NativeEventEmitter = require('./eventemitter.js')
 
 const archive = hyperdrive('./test')
 // Uses runtime.connectNative with a send/on api
-const port = new NativeEventEmitter(`${__dirname}/native.js`)
+// const port = new NativeEventEmitter(`${__dirname}/native.js`)
+const port = runtime.connectNative(`${__dirname}/native.js`)
 
 archive.ready(() => {
-  archive.writeFile('dat.json', JSON.stringify({url: `dat://${archive.key.toString('hex')}`, title: 'test'}), function (err) {
-    rep()
-  })
-})
-
-port.on('error', function(err) {
-  console.error('got err upstream', err)
-})
-
-function rep(cb) {
   const hexKey = archive.key.toString('hex')
-  console.log(hexKey)
-
-  port.on('replicate', function () {
+  archive.writeFile('dat.json', JSON.stringify({url: `dat://${hexKey}`, title: 'test'}), function (err) {
     const stream = archive.replicate({live: true, upload: true, download: true, timeout: 0})
 
-    // Should we send read instructions from the "native"?
-    // port.on('read', function (d) {
-    //   if (d.key !== hexKey) return
-    //   // port.send('read', {key: d.key, data: stream.read(d.data)})
-    // })
-
-    const replicate = new Duplex({
-      read(size) {
-      },
-      write(chunk, encoding, cb) {
-        port.send('write', {
-          key: hexKey,
-          data: chunk
-        })
-
-        port.once('writend', cb)
+    const output = new Transform({
+      transform(chunk, enc, cb) {
+        console.log('message out', chunk.length)
+        port.postMessage(chunk)
+        cb()
       }
     })
 
-    replicate.pipe(stream).pipe(replicate)
+    const input = new PassThrough()
 
-    stream.on('error', (err) => {
-      console.error('err', err.message)
+    port.onMessage.addListener((message) => {
+      const buff = Buffer.from(message)
+      console.log('message in', buff.length)
+      input.write(buff)
     })
 
-    // useful?
-    // stream.on('end', () => {
-    //   console.log('end rep')
-    //   port.send('finalize', {key: hexKey})
-    //   stream.finalize()
-    //   cb && cb()
-    // })
-  })
+    pump(stream, output, function (err) {
+      console.log('out ends')
+    })
 
-  port.send('prepare', hexKey)
-}
+    pump(input, stream, function (err) {
+      console.log('in ends')
+    })
+  })
+})
+
+// port.on('error', function(err) {
+//   console.error('got err upstream', err)
+// })
+//
+// function rep(cb) {
+//   const hexKey = archive.key.toString('hex')
+//   console.log(hexKey)
+//
+//   port.on('replicate', function () {
+//     const stream = archive.replicate({live: true, upload: true, download: true, timeout: 0})
+//
+//     // Should we send read instructions from the "native"?
+//     // port.on('read', function (d) {
+//     //   if (d.key !== hexKey) return
+//     //   // port.send('read', {key: d.key, data: stream.read(d.data)})
+//     // })
+//
+//     const replicate = new Duplex({
+//       read(size) {
+//       },
+//       write(chunk, encoding, cb) {
+//         port.send('write', {
+//           key: hexKey,
+//           data: chunk
+//         })
+//
+//         port.once('writend', cb)
+//       }
+//     })
+//
+//     stream.pipe(replicate)
+//     // replicate.pipe(stream).pipe(replicate)
+//
+//     stream.on('error', (err) => {
+//       console.error('err', err.message)
+//     })
+//
+//     // useful?
+//     // stream.on('end', () => {
+//     //   console.log('end rep')
+//     //   port.send('finalize', {key: hexKey})
+//     //   stream.finalize()
+//     //   cb && cb()
+//     // })
+//   })
+//
+//   port.send('prepare', hexKey)
+// }
--- native.js
+++ native.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 
-const {Duplex} = require('stream')
+const {Duplex, PassThrough, Transform} = require('stream')
 const {toBuf} = require('dat-encoding')
-const {PassThrough} = require('stream')
 const {NativeEncoder, NativeDecoder} = require('browser.runtime')
 const hyperdrive = require('hyperdrive')
 const swarm = require('hyperdiscovery')
@@ -11,86 +10,139 @@ const postMessage = new PassThrough()
 const ram = require('random-access-memory')
 const pump = require('pump')
 
-pump(process.stdin, NativeDecoder(), onmessage, function (err) {})
-pump(postMessage, NativeEncoder(), process.stdout, function (err) {})
+// pump(process.stdin, NativeDecoder(), onmessage, function (err) {})
+// pump(postMessage, NativeEncoder(), process.stdout, function (err) {})
 
-const archives = {}
-const streams = {}
-const swarms = {}
+const key = `39d494a6728654721f5b857030bdccf3c5807ddcf8f62399f0bdadf5a22e3b62`
+const archive = hyperdrive('rep', key, {sparse: false})
+const stream = archive.replicate({live: true, upload: true, download: true, timeout: 0})
 
-function send(event, data) {
-  postMessage.write(JSON.stringify({event: event, data: data}))
-}
+stream.on('error', function (err) {
+  console.error('err st', err)
+})
 
-function init(key) {
-
-  streams[key] = archives[key].replicate({live: true, upload: true, download: true, timeout: 0})
-
-  streams[key].on('error', function (err) {
-    console.error('error rep', err.message)
-  })
-
-  // should we send read instructions?
-  // const replicate = new Duplex({
-  //   read(size) {
-  //     send('read', {key: key, data: size})
-  //   },
-  //   write(chunk, encoding, cb) {
-  //     cb()
-  //   }
-  // })
-  //
-  // replicate.pipe(streams[key]).pipe(replicate)
-
-  swarms[key] = swarm(archives[key])
-  swarms[key].on('connection', (peer, info) => {
-    console.error('Swarm connection')
-  })
-  send('replicate')
-}
-
-onmessage.on('data', function (d) {
-  const {event, data} = JSON.parse(d.toString())
-
-  console.error('Got event %s', event)
-
-  if (event === 'prepare') {
-    try {
-      const key = toBuf(data)
-
-      if (typeof archives[data] === 'undefined') {
-        archives[data] = hyperdrive('rep', key, {sparse: false})
-
-        archives[data].ready(function() {
-          init(data)
-        })
-        return
-      }
-
-      init(data)
-    } catch (e) {
-      send('error', e.message)
-    }
-    return
-  }
-
-  if (event === 'write') {
-    streams[data.key].write(Buffer.from(data.data), null, function() {
-      send('writend')
-    })
-    return
-  }
-
-  // data read from index.js
-  if (event === 'read') {
-    console.error('read', data)
-    // send('read', streams[data.key].read(data.data))
-    return
-  }
-
-  if (event === 'finalize') {
-    // streams[data.key].finalize()
-    return
+const input = new Transform({
+  transform(chunk, enc, cb) {
+    const buffer = Buffer.from(JSON.parse(chunk.toString()))
+    // console.error('in', buffer.length)
+    cb(null, buffer)
   }
 })
 
+const output = new Transform({
+  transform(chunk, enc, cb) {
+    // console.error('out', buffer.length)
+    cb(null, JSON.stringify(chunk))
+  }
+})
+
+
+pump(process.stdin, NativeDecoder(), input, stream, function (err) {
+  console.error('in ends')
+})
+
+pump(stream, output, NativeEncoder(), process.stdout, function (err) {
+  console.error('out ends')
+})
+
+archive.ready(() => {
+  setTimeout(() => {
+    archive.readFile('dat.json', (err, d) => {
+      console.error('test', err, d.toString())
+    })
+  }, 1000)
+})
+
+// pump(stream, NativeEncoder(), process.stdout, function (err) {})
+
+// const archives = {}
+// const streams = {}
+// const swarms = {}
+//
+// function send(event, data) {
+//   postMessage.write(JSON.stringify({event: event, data: data}))
+// }
+//
+// function init(key) {
+//
+//   const stream = archives[key].replicate({live: true, upload: true, download: true, timeout: 0})
+//   streams[key] = new PassThrough()
+//
+//   pump(stream, streams[key], function (err) {
+//     console.log('end rep')
+//   })
+//
+//   streams[key].on('error', function (err) {
+//     console.error('error rep', err.message)
+//   })
+//
+//   // should we send read instructions?
+//   // const replicate = new Duplex({
+//   //   read(size) {
+//   //     send('read', {key: key, data: size})
+//   //   },
+//   //   write(chunk, encoding, cb) {
+//   //     cb()
+//   //   }
+//   // })
+//   //
+//   // replicate.pipe(streams[key]).pipe(replicate)
+//
+//   swarms[key] = swarm(archives[key], {stream: () => stream})
+//   swarms[key].on('connection', (peer, info) => {
+//     console.error('Swarm connection')
+//   })
+//   send('replicate')
+// }
+//
+// onmessage.on('data', function (d) {
+//   const {event, data} = JSON.parse(d.toString())
+//
+//   console.error('Got event %s', event)
+//
+//   if (event === 'prepare') {
+//     try {
+//       const key = toBuf(data)
+//
+//       if (typeof archives[data] === 'undefined') {
+//         archives[data] = hyperdrive('rep', key, {sparse: false})
+//
+//         archives[data].ready(function() {
+//           init(data)
+//         })
+//
+//         setTimeout(() => {
+//           archives[data].readdir('/', function (e, d) {
+//             console.error(e, d)
+//           })
+//         }, 1000)
+//         return
+//       }
+//
+//       init(data)
+//     } catch (e) {
+//       send('error', e.message)
+//     }
+//     return
+//   }
+//
+//   if (event === 'write') {
+//     streams[data.key].write(Buffer.from(data.data), null, function() {
+//       send('writend')
+//     })
+//     return
+//   }
+//
+//   // data read from index.js
+//   if (event === 'read') {
+//     console.error('read', data)
+//     // send('read', streams[data.key].read(data.data))
+//     return
+//   }
+//
+//   if (event === 'finalize') {
+//     // streams[data.key].finalize()
+//     return
+//   }
+// })
+//
