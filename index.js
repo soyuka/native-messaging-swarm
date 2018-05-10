7c585b8ee7633e2349972a61d06108a90bb919d6
soyuka <soyuka@gmail.com>
2018-05-10 22:23:39 +0200

    And done :D

--- index.js
+++ index.js
@@ -5,86 +5,31 @@ const {Duplex, PassThrough, Transform} = require('stream')
 const NativeEventEmitter = require('./eventemitter.js')
 
 const archive = hyperdrive('./test')
-// Uses runtime.connectNative with a send/on api
-// const port = new NativeEventEmitter(`${__dirname}/native.js`)
+const stream = archive.replicate({live: true, upload: true, download: true, timeout: 0})
 const port = runtime.connectNative(`${__dirname}/native.js`)
 
 archive.ready(() => {
   const hexKey = archive.key.toString('hex')
+  port.postMessage({key: hexKey, event: 'prepare'})
+  port.onMessage.addListener((message) => {
+    if (message.event === 'ready') {
+      console.log('[index] ready')
+
+      stream.on('data', function (chunk) {
+        console.log('[index] index => native', chunk.length)
+        port.postMessage({event: 'write', key: hexKey, data: chunk})
+      })
+      return
+    }
+
+    if (message.event === 'write' && hexKey === message.key) {
+      const buff = Buffer.from(message.data)
+      console.log('[index] native => index', buff.length)
+      stream.write(buff)
+      return
+    }
+  })
+
   archive.writeFile('dat.json', JSON.stringify({url: `dat://${hexKey}`, title: 'test'}), function (err) {
-    const stream = archive.replicate({live: true, upload: true, download: true, timeout: 0})
-
-    const output = new Transform({
-      transform(chunk, enc, cb) {
-        console.log('message out', chunk.length)
-        port.postMessage(chunk)
-        cb()
-      }
-    })
-
-    const input = new PassThrough()
-
-    port.onMessage.addListener((message) => {
-      const buff = Buffer.from(message)
-      console.log('message in', buff.length)
-      input.write(buff)
-    })
-
-    pump(stream, output, function (err) {
-      console.log('out ends')
-    })
-
-    pump(input, stream, function (err) {
-      console.log('in ends')
-    })
   })
 })
-
-// port.on('error', function(err) {
-//   console.error('got err upstream', err)
-// })
-//
-// function rep(cb) {
-//   const hexKey = archive.key.toString('hex')
-//   console.log(hexKey)
-//
-//   port.on('replicate', function () {
-//     const stream = archive.replicate({live: true, upload: true, download: true, timeout: 0})
-//
-//     // Should we send read instructions from the "native"?
-//     // port.on('read', function (d) {
-//     //   if (d.key !== hexKey) return
-//     //   // port.send('read', {key: d.key, data: stream.read(d.data)})
-//     // })
-//
-//     const replicate = new Duplex({
-//       read(size) {
-//       },
-//       write(chunk, encoding, cb) {
-//         port.send('write', {
-//           key: hexKey,
-//           data: chunk
-//         })
-//
-//         port.once('writend', cb)
-//       }
-//     })
-//
-//     stream.pipe(replicate)
-//     // replicate.pipe(stream).pipe(replicate)
-//
-//     stream.on('error', (err) => {
-//       console.error('err', err.message)
-//     })
-//
-//     // useful?
-//     // stream.on('end', () => {
-//     //   console.log('end rep')
-//     //   port.send('finalize', {key: hexKey})
-//     //   stream.finalize()
-//     //   cb && cb()
-//     // })
-//   })
-//
-//   port.send('prepare', hexKey)
-// }
--- native.js
+++ native.js
@@ -1,148 +1,64 @@
 #!/usr/bin/env node
 
-const {Duplex, PassThrough, Transform} = require('stream')
+const {PassThrough, Transform} = require('stream')
 const {toBuf} = require('dat-encoding')
 const {NativeEncoder, NativeDecoder} = require('browser.runtime')
 const hyperdrive = require('hyperdrive')
 const swarm = require('hyperdiscovery')
-const onmessage = new PassThrough()
-const postMessage = new PassThrough()
 const ram = require('random-access-memory')
 const pump = require('pump')
 
-// pump(process.stdin, NativeDecoder(), onmessage, function (err) {})
-// pump(postMessage, NativeEncoder(), process.stdout, function (err) {})
+const archives = {}
+const streams = {}
 
-const key = `39d494a6728654721f5b857030bdccf3c5807ddcf8f62399f0bdadf5a22e3b62`
-const archive = hyperdrive('rep', key, {sparse: false})
-const stream = archive.replicate({live: true, upload: true, download: true, timeout: 0})
-
-stream.on('error', function (err) {
-  console.error('err st', err)
-})
-
+const output = new PassThrough()
 const input = new Transform({
   transform(chunk, enc, cb) {
-    const buffer = Buffer.from(JSON.parse(chunk.toString()))
-    // console.error('in', buffer.length)
-    cb(null, buffer)
-  }
-})
+    const buffer = JSON.parse(chunk.toString())
 
-const output = new Transform({
-  transform(chunk, enc, cb) {
-    // console.error('out', buffer.length)
-    cb(null, JSON.stringify(chunk))
-  }
-})
+    // data to write to the replication stream
+    if (buffer.event === 'write') {
+      const data = Buffer.from(buffer.data)
+      console.error('[native] (%s) index => native: ', buffer.key, data.length)
+      streams[buffer.key].write(data)
+      cb()
+      return
+    }
 
+    // prepare event if not write
+    const key = buffer.key
 
-pump(process.stdin, NativeDecoder(), input, stream, function (err) {
-  console.error('in ends')
-})
+    if (archives[key]) {
+      cb()
+      return
+    }
 
-pump(stream, output, NativeEncoder(), process.stdout, function (err) {
-  console.error('out ends')
-})
+    console.error('preparing %s', key)
 
-archive.ready(() => {
-  setTimeout(() => {
-    archive.readFile('dat.json', (err, d) => {
-      console.error('test', err, d.toString())
+    archives[key] = hyperdrive(() => ram(), key, {sparse: false})
+    streams[key] = archives[key].replicate({live: true, upload: true, download: true, timeout: 0})
+    // on data write the data to origin
+    streams[key].on('data', function (chunk) {
+      console.error('[native] (%s) native => index: ', key, chunk.length)
+      output.write(JSON.stringify({key: key, event: 'write', data: chunk}))
     })
-  }, 1000)
+
+    streams[key].on('error', function (err) {
+      console.error('error in replication stream', err.message)
+    })
+
+    archives[key].ready(() => {
+      output.write(JSON.stringify({key: key, event: 'ready'}))
+    })
+
+    cb()
+  }
 })
 
-// pump(stream, NativeEncoder(), process.stdout, function (err) {})
+pump(process.stdin, NativeDecoder(), input, function (err) {
+  console.error('in ends', err)
+})
 
-// const archives = {}
-// const streams = {}
-// const swarms = {}
-//
-// function send(event, data) {
-//   postMessage.write(JSON.stringify({event: event, data: data}))
-// }
-//
-// function init(key) {
-//
-//   const stream = archives[key].replicate({live: true, upload: true, download: true, timeout: 0})
-//   streams[key] = new PassThrough()
-//
-//   pump(stream, streams[key], function (err) {
-//     console.log('end rep')
-//   })
-//
-//   streams[key].on('error', function (err) {
-//     console.error('error rep', err.message)
-//   })
-//
-//   // should we send read instructions?
-//   // const replicate = new Duplex({
-//   //   read(size) {
-//   //     send('read', {key: key, data: size})
-//   //   },
-//   //   write(chunk, encoding, cb) {
-//   //     cb()
-//   //   }
-//   // })
-//   //
-//   // replicate.pipe(streams[key]).pipe(replicate)
-//
-//   swarms[key] = swarm(archives[key], {stream: () => stream})
-//   swarms[key].on('connection', (peer, info) => {
-//     console.error('Swarm connection')
-//   })
-//   send('replicate')
-// }
-//
-// onmessage.on('data', function (d) {
-//   const {event, data} = JSON.parse(d.toString())
-//
-//   console.error('Got event %s', event)
-//
-//   if (event === 'prepare') {
-//     try {
-//       const key = toBuf(data)
-//
-//       if (typeof archives[data] === 'undefined') {
-//         archives[data] = hyperdrive('rep', key, {sparse: false})
-//
-//         archives[data].ready(function() {
-//           init(data)
-//         })
-//
-//         setTimeout(() => {
-//           archives[data].readdir('/', function (e, d) {
-//             console.error(e, d)
-//           })
-//         }, 1000)
-//         return
-//       }
-//
-//       init(data)
-//     } catch (e) {
-//       send('error', e.message)
-//     }
-//     return
-//   }
-//
-//   if (event === 'write') {
-//     streams[data.key].write(Buffer.from(data.data), null, function() {
-//       send('writend')
-//     })
-//     return
-//   }
-//
-//   // data read from index.js
-//   if (event === 'read') {
-//     console.error('read', data)
-//     // send('read', streams[data.key].read(data.data))
-//     return
-//   }
-//
-//   if (event === 'finalize') {
-//     // streams[data.key].finalize()
-//     return
-//   }
-// })
-//
+pump(output, NativeEncoder(), process.stdout, function (err) {
+  console.error('out ends', err)
+})
